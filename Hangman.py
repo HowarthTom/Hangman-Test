33a68ceab7306a873af08e2260aad5edf3ec2f18
Tom Howarth <tom@MacBook-Pro.home>
2022-09-26 20:21:00 +0100

    Created the hangman class and methods

---
+++ Hangman.py
@@ -0,0 +1,20 @@
+import random
+
+class Hangman:
+
+    def __init__(self, word_list, num_lives=5):
+        pass
+
+    def check_letter(self, letter):
+        pass
+
+    def ask_letter(self):
+        pass
+    
+def play_game(word_list):
+    game = Hangman(word_list, num_lives=5)
+    pass
+
+if __name__ == '__main__':
+    word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
+    play_game(word_list)
